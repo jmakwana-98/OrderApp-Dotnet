31304e92e05be4479166fc06734c89820fa2815d
jmakwana-98 <jeenal98makwana@gmail.com>
2024-06-06 21:52:19 +0530

    Create Order.cs

---
+++ OrderApplication/Models/Order.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace OrderApplication.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+
+        [MaxLength(20)]
+        public string? FirstName { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string? LastName { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string? Description { get; set; }
+
+        [Range(1, 20)]
+        public int Quantity { get; set; }
+    }
+}
