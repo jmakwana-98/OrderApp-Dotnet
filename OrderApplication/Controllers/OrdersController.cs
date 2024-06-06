4dcb7b8378c2bee3e7383317e1c3ba92cacd40f0
jmakwana-98 <jeenal98makwana@gmail.com>
2024-06-06 21:48:44 +0530

    Add files via upload

---
+++ OrderApplication/Controllers/OrdersController.cs
@@ -0,0 +1,67 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OrderApplication.Models;
+using OrderApplication.MySQL;
+
+namespace OrderApplication.Controllers
+{
+    [Route("api/v1")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private readonly OrderContext _context;
+
+        public OrdersController(OrderContext context)
+        {
+            _context = context;
+        }
+
+        // GET
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Order>>> GetOrders()
+        {
+            return await _context.Orders.ToListAsync();
+        }
+
+        // GET
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Order>> GetOrder(int id)
+        {
+            var order = await _context.Orders.FindAsync(id);
+
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            return order;
+        }
+
+        // POST
+        [HttpPost]
+        public async Task<ActionResult<Order>> PostOrder(Order order)
+        {
+            _context.Orders.Add(order);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetOrder), new { id = order.Id }, order);
+        }
+
+        // DELETE
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteOrder(int id)
+        {
+            var order = await _context.Orders.FindAsync(id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            _context.Orders.Remove(order);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+
+}
