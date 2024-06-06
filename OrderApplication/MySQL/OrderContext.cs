7e203a868146408bcc2e98ba98954fbe19c6492e
jmakwana-98 <jeenal98makwana@gmail.com>
2024-06-06 21:53:09 +0530

    Create OrderContext.cs

---
+++ OrderApplication/MySQL/OrderContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using OrderApplication.Models;
+
+namespace OrderApplication.MySQL
+{
+    public class OrderContext : DbContext
+    {
+        public OrderContext(DbContextOptions<OrderContext> options) : base(options) { }
+
+        public DbSet<Order> Orders { get; set; }
+    }
+}
