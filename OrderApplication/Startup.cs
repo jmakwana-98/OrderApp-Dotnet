a2ee5c93e9ab02e6a3d5f7a17ee975dca93a7adb
jmakwana-98 <jeenal98makwana@gmail.com>
2024-06-06 21:51:37 +0530

    Add files via upload

---
+++ OrderApplication/Startup.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrderApplication.MySQL;
+
+namespace OrderApplication
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<OrderContext>(opt => opt.UseInMemoryDatabase("OrderList"));
+            services.AddControllers();
+            services.AddSwaggerGen();
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "OrderAPI v1"));
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+        }
+
+    }
+}
